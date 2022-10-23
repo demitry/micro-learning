bcdcfcce2a8740bda96f72fceac3d659fffa0736
Dmitry Poluektov <dpoluektov@gmail.com>
2022-10-23 13:19:54 +0300

    Initial LinqOrderBenchmark

---
+++ LinqOrderBenchmark/LinqOrderBenchmark/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Linq Order Benchmark");
+
+var random = new Random(420);
+
+var items = Enumerable.Range(1, 100).Select(x => random.Next());
+
+var ordered = items.OrderBy(x => x).ToArray();
+
+var orderedDesc = items.OrderByDescending(x => x).ToArray();
