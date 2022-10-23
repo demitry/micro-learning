d6dfd086ad98f6b7194e63f9667dce9065718700
Dmitry Poluektov <dpoluektov@gmail.com>
2022-10-23 16:08:10 +0300

    Arrays and List Order Benchmarks

---
+++ LinqOrderBenchmark/LinqOrderBenchmark/Benchmarks.cs
@@ -0,0 +1,86 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqOrderBenchmark
+{
+    [MemoryDiagnoser(false)]
+    public class ArrayBenchmarks
+    {
+        private readonly Random _random = new(420);
+
+        private int[] _items;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _items = Enumerable.Range(0, 100).Select(_ => _random.Next()).ToArray();
+        }
+
+        [Benchmark]
+        public int[] OrderBy()
+        {
+            var items = Enumerable.Range(0, 100).Select(_ => _random.Next()).ToArray();
+            return items.OrderBy(x => x).ToArray();
+        }
+
+        [Benchmark]
+        public int[] Order()
+        {
+            var items = Enumerable.Range(0, 100).Select(_ => _random.Next()).ToArray();
+            return items.Order().ToArray();
+        }
+
+        [Benchmark]
+        public int[] Sort()
+        {
+            var items = Enumerable.Range(0, 100).Select(_ => _random.Next()).ToArray();
+            Array.Sort(items);
+            return items;
+        }
+
+        [Benchmark]
+        public int[] SpanSort()
+        {
+            Span<int> spanItems = Enumerable.Range(0, 100).Select(_ => _random.Next()).ToArray();
+            spanItems.Sort();
+            return spanItems.ToArray();
+        }
+        //[Benchmark]
+        //public int[] OrderByDesc() => _items.OrderByDescending(x => x).ToArray();
+
+        //[Benchmark]
+        //public int[] OrderDesc() => _items.OrderDescending().ToArray();
+
+    }
+
+    [MemoryDiagnoser(false)]
+    public class ListBenchmarks
+    {
+        private readonly Random _random = new(420);
+
+        private List<int> _items;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _items = Enumerable.Range(0, 100).Select(_ => _random.Next()).ToList();
+        }
+
+        [Benchmark]
+        public int[] OrderBy() => _items.OrderBy(x => x).ToArray();
+
+        [Benchmark]
+        public int[] Order() => _items.Order().ToArray();
+
+        [Benchmark]
+        public int[] OrderByDesc() => _items.OrderByDescending(x => x).ToArray();
+
+        [Benchmark]
+        public int[] OrderDesc() => _items.OrderDescending().ToArray();
+
+    }
+}
--- LinqOrderBenchmark/LinqOrderBenchmark/Program.cs
+++ LinqOrderBenchmark/LinqOrderBenchmark/Program.cs
@@ -1,9 +1,38 @@
-﻿Console.WriteLine("Linq Order Benchmark");
+﻿//Console.WriteLine("Linq Order Benchmark");
+//var random = new Random(420);
+//var items = Enumerable.Range(1, 100).Select(x => random.Next());
+//var ordered = items.OrderBy(x => x).ToArray();
+//var orderedDesc = items.OrderByDescending(x => x).ToArray();
 
-var random = new Random(420);
+using BenchmarkDotNet.Running;
+using LinqOrderBenchmark;
 
-var items = Enumerable.Range(1, 100).Select(x => random.Next());
+// BenchmarkRunner.Run<ArrayBenchmarks>();
 
-var ordered = items.OrderBy(x => x).ToArray();
+/*
+| Method      | Mean     | Error     | StdDev    | Allocated |
+| ----------  | ---------| ----------| ----------| --------- |
+| OrderBy     | 6.176 us | 0.0572 us | 0.0507 us | 1.83 KB   |
+| Order       | 6.173 us | 0.0214 us | 0.0167 us | 1.41 KB   |
+| OrderByDesc | 6.904 us | 0.0333 us | 0.0295 us | 1.83 KB   |
+| OrderDesc   | 6.724 us | 0.0631 us | 0.0590 us | 1.41 KB   |
+*/
 
-var orderedDesc = items.OrderByDescending(x => x).ToArray();
+/*
+| Method    | Mean      | Error     | StdDev    | Allocated |
+| --------- | ---------:| ----------:| --------:| ---------:|
+| OrderBy   | 9.483 us  | 0.0176 us | 0.0156 us | 2448 B |
+| Order     | 9.227 us  | 0.0155 us | 0.0130 us | 2024 B |
+| Sort      | 4.129 us  | 0.0113 us | 0.0101 us | 576 B  |
+| SpanSort  | 4.277 us  | 0.0164 us | 0.0153 us | 1000 B |
+*/
+
+BenchmarkRunner.Run<ListBenchmarks>();
+/*
+| Method        | Mean | Error | StdDev | Allocated |
+| ------------  | ---------:| ----------:| ----------:| ----------:|
+| OrderBy       | 6.172 us | 0.0639 us | 0.0597 us | 1.83 KB |
+| Order         | 6.238 us | 0.0497 us | 0.0465 us | 1.41 KB |
+| OrderByDesc   | 6.865 us | 0.0433 us | 0.0384 us | 1.83 KB |
+| OrderDesc     | 6.537 us | 0.0437 us | 0.0409 us | 1.41 KB |
+*/
